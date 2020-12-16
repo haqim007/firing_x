c991af82b8b06d6889d5543b07d0aa0b316bc959
Haqim <khaqim414@gmail.com>
2020-12-16 12:51:07 +0700

    Fixing main.go and build binary file

--- main.go
+++ main.go
@@ -11,57 +11,59 @@ import (
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Println("> Start ")
 	var arrInput [8][8]string
-	for i := 0; i < 8; i++ {
-		for j := 0; j < 8; j++ {
-			arrInput[i][j] = " "
+	for {
+		fmt.Println("")
+		fmt.Println("> Start ")
+		for i := 0; i < 8; i++ {
+			for j := 0; j < 8; j++ {
+				arrInput[i][j] = " "
+			}
 		}
-	}
 
-	for i := 0; i < 8; i++ {
+		for i := 0; i < 8; i++ {
 
-		fmt.Println("> Masukkan Bidak", i+1, " (x,y):")
-		fmt.Print("> ")
-		cmdString, err := reader.ReadString('\n')
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
+			fmt.Println("> Masukkan Bidak", i+1, " (x,y):")
+			fmt.Print("> ")
+			cmdString, err := reader.ReadString('\n')
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+			} else {
+				inputStr := strings.TrimSuffix(cmdString, "\n")
+				arrInputStr := strings.Fields(inputStr)
+
+				if arrInputStr[0] == "exit" {
+					os.Exit(0)
+				}
+
+				isValid := mf.ValidateCoordinate(arrInputStr[0])
+				if isValid != nil {
+					fmt.Println(isValid)
+					i--
+					continue
+				}
+				res, errRes := mf.ConvertCoordinate(arrInputStr[0])
+				if errRes != nil {
+					fmt.Println(errRes)
+					os.Exit(0)
+				}
+
+				arrInput[res[0]-1][res[1]-1] = "x"
+
+			}
+		}
+		resGetVictim, errGetVictim := mf.GetVictim(arrInput)
+		if errGetVictim != nil {
+			fmt.Println(errGetVictim)
 		} else {
-			inputStr := strings.TrimSuffix(cmdString, "\n")
-			arrInputStr := strings.Fields(inputStr)
-
-			if arrInputStr[0] == "exit" {
-				os.Exit(0)
+			fmt.Print("> Hasil: ")
+			for i := range resGetVictim {
+				if resGetVictim[i] != "" {
+					fmt.Print(resGetVictim[i], " ")
+				}
 			}
-
-			isValid := mf.ValidateCoordinate(arrInputStr[0])
-			if isValid != nil {
-				fmt.Println(isValid)
-				i--
-				continue
-			}
-			res, errRes := mf.ConvertCoordinate(arrInputStr[0])
-			if errRes != nil {
-				fmt.Println(errRes)
-				os.Exit(0)
-			}
-
-			arrInput[res[0]-1][res[1]-1] = "x"
-
+			fmt.Print("\n")
 		}
+		mf.ChessBoard(arrInput)
 	}
-	resGetVictim, errGetVictim := mf.GetVictim(arrInput)
-	if errGetVictim != nil {
-		fmt.Println(errGetVictim)
-	} else {
-		fmt.Print("> Hasil: ")
-		for i := range resGetVictim {
-			if resGetVictim[i] != "" {
-				fmt.Print(resGetVictim[i], " ")
-			}
-		}
-		fmt.Print("\n")
-	}
-	fmt.Println(arrInput)
-	mf.ChessBoard(arrInput)
 }
